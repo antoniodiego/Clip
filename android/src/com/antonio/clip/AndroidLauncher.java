3bbd61e6e2a221b1c749b8df8f654203bc7c050f
Antonio Diêgo Silva <antoniodiegoluz@hotmail.com>
2023-01-05 21:10:36 -0300

    first commit

---
+++ android/src/com/antonio/clip/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.antonio.clip;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.antonio.clip.Clip;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Clip(), config);
+	}
+}
---
+++ desktop/src/com/antonio/clip/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.antonio.clip;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.antonio.clip.Clip;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Clip");
+		new Lwjgl3Application(new Clip(), config);
+	}
+}
